b18d6e290882b6da02cfa628e31dc2bf5a4f284d
Faris Riky Pratama <farisriky01@gmail.com>
2025-11-16 21:29:10 +0700

    Create a data structure for user (user_model.py) and add __init__.py each of folders.

---
+++ models/user_model.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+class User:
+    sequence = 0
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+        User.sequence += 1
+        today = datetime.now().strftime('%Y%m%d')
+        self.user_id = f"{today}{User.sequence:03d}"
+
+    # To get instance property in dictionary form
+    def get_data(self):
+        return {
+            'user_id': self.user_id,
+            'username': self.username,
+            'password': self.password
+        }
