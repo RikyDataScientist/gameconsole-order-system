af8ab8ff88956bc7088028a9d1cd1c84a7f2f6f8
RikyDataScientist <farisriky01@gmail.com>
2025-12-01 21:11:26 +0700

    Code adjustment for room model and create room controller

---
+++ controller/room_controller.py
@@ -0,0 +1,41 @@
+from models.room_model import Room
+from utils.json_helpers import load_data, save_data
+
+class RoomManager:
+    path = "database/rooms.json"
+
+    def __init__(self, room_id, name_room):
+        self.model = Room(room_id, name_room)
+        self.data = self.load()
+        self.time_in_room = None
+        self.booked_times = None
+
+    def load(self):
+        data = load_data(RoomManager.path)
+        for value in data:
+            if self.model.room_id == value["room_id"]:
+                self.booked_times = value["booked_times"]
+                self.time_in_room = [i for i in self.model.time_each_rooms if i not in self.booked_times]
+                return value
+        return None
+
+    def check_room_general(self):
+        if self.time_in_room > 0:
+            return True
+        else:
+            return False
+
+    def check_room_by_hour(self, selected_time):
+        return all(t in self.time_in_room for t in selected_time)
+
+    def order(self, selected_time):
+        for value in selected_time:
+            if value in self.time_in_room:
+                self.booked_times.append(value)
+                self.time_in_room.remove(value)
+
+        data = load_data(RoomManager.path)
+        for a in data:
+            if self.model.room_id == a["room_id"]:
+                a.update(self.model.get_data(self.time_in_room, self.booked_times))
+        save_data(RoomManager.path, data)
--- models/room_model.py
+++ models/room_model.py
@@ -3,10 +3,11 @@ from datetime import time
 class Room:
 
     times_available = [
-        time(8),
         time(9),
         time(10),
         time(11),
+        time(12),
+        time(13),
         time(14),
         time(15),
         time(16),
@@ -19,16 +20,9 @@ class Room:
         self.time_each_rooms = Room.times_available.copy()  # Copy time availability for each rooms.
         self.booked_times = []
 
-    def check_availability(self, selected_time):
-        return all(t in self.time_each_rooms for t in selected_time)
-
-    def order(self, selected_time):
-        for value in selected_time:
-            if value in self.time_each_rooms:
-                self.time_each_rooms.remove(value)
-                self.booked_times.append(value)
-
-    def get_data(self):
+    def get_data(self, times, booked):
+        self.time_each_rooms = times
+        self.booked_times = booked
         return {
             "room_id": self.room_id,
             "room_name": self.name_room,
