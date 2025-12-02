82b7f6cae325f3f34098d2f4f81dde84f6bfa6e0
RikyDataScientist <farisriky01@gmail.com>
2025-12-02 21:19:35 +0700

    Create booking controller

---
+++ controller/booking_controller.py
@@ -0,0 +1,43 @@
+from models.booking import Booking
+from utils.json_helpers import load_data, save_data
+
+class BookingController:
+    console = {
+        "PS5": 15000,
+        "PS4": 10000,
+        "Xbox Series X": 20000,
+        "Nintendo Switch": 18000
+    }
+
+    def __init__(self, user, room):
+        self.user = user
+        self.room = room
+
+    def price(self, console, time_selected):
+        price = BookingController.console[console]
+        return price * len(time_selected)
+
+    def create_booking(self, console, time_selected):
+        if not console:
+            raise ValueError("Pilih Jenis Console")
+        if not time_selected:
+            raise ValueError("Pilih Waktu Bermain")
+
+        time_in_strings = [a.strftime("%H:%M") for a in time_selected]
+
+        price = self.price(console, time_selected)
+
+        data_booking = Booking(
+            username=self.user.username,
+            room_id=self.room.room_id,
+            times=time_in_strings,
+            console=console,
+            price=price
+        )
+
+        path = "/database/booking.json"
+        data = load_data(path)
+        data.append(data_booking.get_dict())
+        save_data(path, data)
+
+        return data_booking
